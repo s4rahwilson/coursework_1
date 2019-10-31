8dc571dae3b4ec814d073576a9b498e914cab32f
Sarah Wilson <sarahwilson123@icloud.com>
2019-10-31 17:05:29 +0000

    first commit - added maven project

---
+++ src/test/java/Dec2HexTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class Dec2HexTest {
+
+
+    @Test
+    public void test() throws Exception {
+
+    }
+
+}
