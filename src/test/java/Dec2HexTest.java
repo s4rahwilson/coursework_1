0b7a4fddacc08d8ac86d6cc594a1349da8fbab3b
Sarah Wilson <sarahwilson123@icloud.com>
2019-11-05 15:09:15 +0000

    first commit - changed main class into methods and added test class

--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -1,14 +1,15 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 
 public class Dec2HexTest {
 
 
     @Test
-    public void test() throws Exception {
+    public void input() throws Exception {
 
+        
     }
 
 }
